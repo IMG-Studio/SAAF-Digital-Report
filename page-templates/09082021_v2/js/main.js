071a074e6af92a09bb2edd9ffa3833e711424d15
Enrique Urbina <btace1347@yahoo.com>
2021-09-22 15:15:38 -0500

    fixed

--- page-templates/09082021_v2/js/main.js
+++ page-templates/09082021_v2/js/main.js
@@ -999,6 +999,7 @@ window.addEventListener("DOMContentLoaded", event =>
 				){
 					console.log("toggle")
 					$('.header-left-nav').toggleClass('toggle');
+					$('.menu-btn').toggleClass('active');
 
 				}
 
