e6c0c002b08d53df03bd66ffc6f05f1287fc738c
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-09-28 14:30:59 -0500

    adjusted section 15 image

--- page-templates/update_09082021_v2.php
+++ page-templates/update_09082021_v2.php
@@ -947,7 +947,7 @@
     </section>
     <section class="section-15">
         <div class="section-15__image">
-            <img src="<?php echo get_template_directory_uri(); ?>/page-templates/09082021_v2/images/section-15.jpg">
+            <img src="<?php echo get_template_directory_uri(); ?>/page-templates/09082021_v2/images/section-15rev.jpg">
             <div class="section-15__imageshorttext">» San Antonio Oasis </div>
         </div>
         <div class="section-14__text-wrapper">
